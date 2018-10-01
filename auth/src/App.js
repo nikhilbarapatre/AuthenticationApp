37dc67827cb9ecbe100da784290d58869efa2a19
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-10-01 00:02:24 -0700

    login functionality added

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import firebase from "firebase";
+import { Header } from "./components/common";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBP5Sk5TkWRwiJbGgpOelXxXKaj1u6XX8M",
+      authDomain: "authentication-4544f.firebaseapp.com",
+      databaseURL: "https://authentication-4544f.firebaseio.com",
+      projectId: "authentication-4544f",
+      storageBucket: "authentication-4544f.appspot.com",
+      messagingSenderId: "971320507794"
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An App!</Text>
+        <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
