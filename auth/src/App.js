910c8f7cce251ada13be434829f2df39ac341957
nikhilbarapatre <nbarapatre@dons.usfca.edu>
2018-10-02 23:58:59 -0700

    final commit

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import firebase from "firebase";
-import { Header } from "./components/common";
+import { Header, Button, Spinner } from "./components/common";
 import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+  state = { loggedIn: null };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: "AIzaSyBP5Sk5TkWRwiJbGgpOelXxXKaj1u6XX8M",
@@ -14,13 +15,38 @@ class App extends Component {
       storageBucket: "authentication-4544f.appspot.com",
       messagingSenderId: "971320507794"
     });
+    //Here, we will know if the user signs in or signs out. In other words, this eventHandler for handling
+    //either signing in or signing out.
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ height: 45, marginTop: 10 }}>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- auth/src/components/common/Header.js
+++ auth/src/components/common/Header.js
@@ -21,7 +21,7 @@ const styles = {
     paddingTop: 15,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
+    shadowOpacity: 0.9,
     elevation: 2,
     position: "relative"
   },
